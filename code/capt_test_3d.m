90dcf5719be6e9915c52203f883e05d82d63591a
Justin Starr <starrj@seas1288.wireless-pennnet.upenn.edu>
2015-04-24 15:33:00 -0400

    point seeding and final time complete

--- code/capt_test_3d.m
+++ code/capt_test_3d.m
@@ -5,13 +5,15 @@ addpath('munkres')
 
 dim = 3;
 
-N = 10;
-M = 10;
+N = 5;
+M = 5;
 r_base = .5;
 
 R = [r_base; r_base; 4*r_base];
 
 max_v = 1;
+max_a = 1;
+
 t0 = 0;
 max_allow_dist = 2*r_base*sqrt(2);
 
@@ -56,24 +58,32 @@ fprintf('Done! \n')
 
 fprintf('Running the Hungarian algorithm... ')
 [assignment, cost] = munkres(D);
-lin_idx = sub2ind([N,M],1:N,assignment);
-max_d = max(sqrt(D(lin_idx)));
 fprintf('Done! \n')
 
-tf = max_d/max_v;
-
 G = G_pre(assignment,:);
+
+lin_idx = sub2ind([N,M],1:N,assignment);
+max_d = max(sqrt(D(lin_idx)));
+
+[~,ind] = min(abs(D(:) - max_d^2));
+[ind_i, ind_j] = ind2sub([N,M],ind);
+raw_dist = (G_pre(ind_j,:)-S(ind_i,:));
+raw_dist = raw_dist.* [1,1,2.2];
+max_d_a = sqrt(sum((raw_dist.^2)));
+
+tf = sqrt(max_d_a*5.773)/max_a + .1*(max_d_a)^(1/3);
+
 toc
 
 fprintf('Plotting... ')
 f = figure();
-% plot_phase1(S,G,t0,tf,R)
-hold on
-grid on
-plot3(S(:,1),S(:,2),S(:,3),'b.')
-plot3(G(:,1),G(:,2),G(:,3),'r.')
-axis equal
-pause
-delete(f)
+plot_phase1(S,G,t0,tf,R)
+% hold on
+% grid on
+% plot3(S(:,1),S(:,2),S(:,3),'b.')
+% plot3(G(:,1),G(:,2),G(:,3),'r.')
+% axis equal
+% pause
+% delete(f)
 fprintf('Done! \n')
 
