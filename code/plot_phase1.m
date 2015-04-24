7447414289096d81f5d2d5e8f1d5c29ba98dc837
Justin <justin@jyim-E6510.(none)>
2015-04-24 13:24:02 -0400

    plot_phase1 constraint checks and trajectory line display fix

--- code/plot_phase1.m
+++ code/plot_phase1.m
@@ -1,4 +1,7 @@
 function [] = plot_phase1(S,G,t0,tf,r)
+% Plot and check results for simple 2D, min velocity^2 C-CAPT.
+% ASSUMPTIONS:
+%   Velocity constant (for velocity verification)
 
 N = size(S,1);
 
@@ -13,7 +16,8 @@ robot_pts = r*[cos(theta'), sin(theta')];
 figure(1)
 clf
 
-plot(reshape([S(:,1)';G(:,1)'],[2*N,1]), reshape([S(:,2)';G(:,2)'],[2*N,1]),'+-k');
+plot(reshape([S(:,1)';G(:,1)';NaN(1,N)],[3*N,1]), ...
+    reshape([S(:,2)';G(:,2)';NaN(1,N)],[3*N,1]),'+-k');
 
 hold on
 h_robots = zeros(N,1);
@@ -31,16 +35,29 @@ title('C-CAPT Phase 1: 2D, Velocity Squared')
 
 waitforbuttonpress
 
+% Check velocities
+max_v = max(sqrt(sum((G-S).^2,2))/(tf-t0));
+disp(['Max velocity: ',num2str(max_v)]);
+
 % Animate the motion
 n_frames = 30;
 dur = 1;
 t = linspace(t0,tf,n_frames);
 tic
 for ii = 1:n_frames
+    pos = (1-beta(t(ii)))*S + beta(t(ii))*G;
     for jj = 1:N
-        pos = (1-beta(t(ii)))*S(jj,:) + beta(t(ii))*G(jj,:);
-        set(h_robots(jj),'xdata', pos(1) + robot_pts(:,1), ...
-            'ydata', pos(2) + robot_pts(:,2));
+        dists = sqrt(sum(bsxfun(@minus,pos([1:(jj-1),(jj+1):N],:),pos(jj,:)).^2,2));
+        collide = dists < 2*r;
+        set(h_robots(jj),'xdata', pos(jj,1) + robot_pts(:,1), ...
+            'ydata', pos(jj,2) + robot_pts(:,2));
+        if any(collide)
+            colliders = find(collide);
+            colliders(colliders >= jj) = colliders(colliders >= jj) + 1;
+            set(h_robots([colliders,jj]),'FaceColor',[1,0,0])
+            disp(['COLLISION AT t=',num2str(t)]);
+            return
+        end
     end
     drawnow
     pause(dur/n_frames - toc);
