997010e42bf616a2c8721f174f22873d315e7cb5
James Yang <james.yang92@gmail.com>
2015-04-25 11:23:47 -0400

    added nth derivative trajectory generator to plot_3d.m

--- code/traj_tester.m
+++ code/traj_tester.m
@@ -1,6 +1,6 @@
 %clear all
 close all
-
+%{
 X = [-1 0 0;...
     0 2 0;...
     1 0 0];
@@ -10,17 +10,19 @@ X_0 = [-1 -5 0;...
 X_f = [0 0 0;...
         0 0 0;...
         0 0 0];
+%}
+X = [-1 0 1]';
+X_0 = [-1 0 0]';
+X_f = [0 0 0]';
 ts = [0 5 6]';
 n = 4;
 C = min_n_traj(X,X_0,X_f,n,ts);
 
 time = (0:0.001:ts(end))';
-pos = zeros(length(time),3);
-vel = zeros(length(time),3);
-acc = zeros(length(time),3);
-jerk = zeros(length(time),3);
-c_idx = 1;
-coeff = C(1:2*n,:);
+pos = zeros(length(time),1);
+vel = zeros(length(time),1);
+acc = zeros(length(time),1);
+jerk = zeros(length(time),1);
 
 for i = 1:length(time)
     t = time(i);
@@ -38,7 +40,9 @@ plot(time,vel(:,1),'g-')
 subplot(3,1,3)
 plot(time,acc(:,1),'b-')
 
+%{
 figure(2)
 plot(pos(:,1),pos(:,2),'b-')
 axis equal
+%}
 grid on
