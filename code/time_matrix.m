d6b17aac81bfe0c80ac693ac8c3860d4d52467df
James Yang <james.yang92@gmail.com>
2015-04-25 10:53:51 -0400

    made time matrix to spit out up to 2*n-1 derivatives

--- code/time_matrix.m
+++ code/time_matrix.m
@@ -1,8 +1,8 @@
 function T = time_matrix(t,n)
 
-T = zeros(3,2*n);
+T = zeros(2*n);
 
-for i = 1:3
+for i = 1:2*n
     den = 1-i:2*n-i;
     d = factorial(0:2*n-1)./factorial(den.*(den>0));
     mask = [zeros(1,i-1) ones(1,2*n-i+1)];
